4417a7db6b83aa9418486c389ab8b6f03ae5e698
Constance <constance.yvon@yahoo.fr>
2020-10-12 15:01:54 +0200

    serveur TCP V1

--- TP1/TCPServeur.java
+++ TP1/TCPServeur.java
@@ -1,15 +1,25 @@
-import java.net.ServerSocket;
-import java.io.IOException;
+import java.net.*;
+import java.io.*;
 
 public class TCPServeur{
 
   public static void main(String[] args){
-    private int port = 1027;
-    private ServerSocket serveur = null;
+    int port = 1027;
+    ServerSocket serveur = null;
     try {
       serveur = new ServerSocket(port);
     } catch (IOException e) {
       System.err.println("Echec création socket");
+    }
+    while(true){
+      try {
+        Socket connection = serveur.accept();
+        OutputStreamWriter out = new OutputStreamWriter(connection.getOutputStream( ));
+        out.write("Connecté:" +connection+"\r\n");
+        connection.close();
+      } catch (IOException e) {
+        System.err.println("Echec connection");
+      }
     }
   }
 
