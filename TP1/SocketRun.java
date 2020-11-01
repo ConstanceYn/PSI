7cb83a7a9a940110b068d97ab7fd8e5a3c871ecd
Constance <constance.yvon@yahoo.fr>
2020-11-01 17:35:47 +0100

    mini correction

--- TP1/SocketRun.java
+++ TP1/SocketRun.java
@@ -14,37 +14,34 @@ public class SocketRun implements Runnable {
     try{
       cmp.incr();
       boolean continuer = true;
-      while(continuer){
-        try {
-          PrintWriter pw = new PrintWriter(connection.getOutputStream() );
-          BufferedReader read = new BufferedReader( new InputStreamReader(connection.getInputStream()));
-          while(true)
-          {
-            String content = cmp.Cmp_to_String();
-            System.out.print(content);
-            // envoie du nombre de connections
-            pw.print(content);
-            pw.flush();
-            // lit le message d'acquitement du client
-            int co = read.read();
-            // si pas d'acquitement, le client est considéré comme déconnecté
-            if (co == -1){
-              System.out.println("co = -1");
-              continuer = false;
-              cmp.decr();
-              connection.close();
-              break;
-            }
-            else {
-              Thread.sleep(10000);
-            }
+      try {
+        PrintWriter pw = new PrintWriter(connection.getOutputStream() );
+        BufferedReader read = new BufferedReader( new InputStreamReader(connection.getInputStream()));
+        while(continuer)
+        {
+          String content = cmp.Cmp_to_String();
+          System.out.print(content);
+          // envoie du nombre de connections
+          pw.print(content);
+          pw.flush();
+          // lit le message d'acquitement du client
+          int co = read.read();
+          // si pas d'acquitement, le client est considéré comme déconnecté
+          if (co == -1){
+            System.out.println("co = -1");
+            cmp.decr();
+            connection.close();
+            continuer = false;
+            break;
+          } else {
+            Thread.sleep(10000);
           }
-        } catch (IOException e) {
-          // Client considérer déconnecté
-          System.err.println("Echec connection write");
-          continuer = false;
-          cmp.decr();
         }
+      } catch (IOException e) {
+        // Client considéré déconnecté
+        System.err.println("Echec connection write");
+        cmp.decr();
+        continuer = false;
       }
     }catch(InterruptedException e){
     }
